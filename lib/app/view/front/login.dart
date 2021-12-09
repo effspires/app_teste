22452f3a426980a15272fb3e80d4c9e71c2076fb
effspires <ef.fspires@gmail.com>
2021-12-09 17:28:50 -0300

    VS_Final_3

--- lib/app/view/front/login.dart
+++ lib/app/view/front/login.dart
@@ -39,140 +39,154 @@ class _LoginState extends State<Login> {
     _loginBack.getContext(context);
     return SafeArea(
       child: Scaffold(
-        body: Container(
+        /*body: Container(
           alignment: Alignment.center,
-          margin: const EdgeInsets.all(32),
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5),
-            ),
-            elevation: 16,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Center(
-                    child: SizedBox(
-                        width: 200,
-                        height: 150,
-                        child: Image.asset('assets/images/voalle-logo.png'),
-                    ),
-                  ),
-                  Observer(
-                    builder: (_) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.grey[200],
+          margin: const EdgeInsets.all(32),*/
+        body: Container(
+          padding: const EdgeInsets.all(64),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 32),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(5),
                         ),
-                        padding: const EdgeInsets.only(left: 16),
-                        child: TextField(
-                          decoration: const InputDecoration(
-                            hintText: 'E-mail',
-                            border: InputBorder.none,
-                            prefixIcon: Icon(Icons.account_circle, color: Color(0xff237e84),),
+                        elevation: 16,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Center(
+                                child: SizedBox(
+                                    width: 200,
+                                    height: 150,
+                                    child: Image.asset('assets/images/voalle-logo.png'),
+                                ),
+                              ),
+                              Observer(
+                                builder: (_) {
+                                  return Container(
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.rectangle,
+                                      color: Colors.grey[200],
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                    padding: const EdgeInsets.only(left: 16),
+                                    child: TextField(
+                                      decoration: const InputDecoration(
+                                        hintText: 'E-mail',
+                                        border: InputBorder.none,
+                                        prefixIcon: Icon(Icons.account_circle, color: Color(0xff237e84),),
+                                      ),
+                                      keyboardType: TextInputType.emailAddress,
+                                      onChanged: _loginBack.setEmail,
+                                      enabled: !_loginBack.loading,
+                                    ),
+                                  );
+                                },
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Observer(
+                                builder: (_) {
+                                  return Container(
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.rectangle,
+                                      color: Colors.grey[200],
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                    padding: const EdgeInsets.only(left: 16),
+                                    child: TextField(
+                                      decoration: InputDecoration(
+                                        hintText: 'Senha',
+                                        border: InputBorder.none,
+                                        prefixIcon: const Icon(Icons.lock, color: Color(0xff237e84)),
+                                        suffixIcon: IconButton(
+                                          icon: _loginBack.passVisible
+                                              ? const Icon(Icons.visibility_off, color: Color(0xff237e84))
+                                              : const Icon(Icons.visibility, color: Color(0xff237e84)),
+                                          onPressed: _loginBack.togglePassVisible,
+                                        ),
+                                      ),
+                                      obscureText: !_loginBack.passVisible,
+                                      onChanged: _loginBack.setSenha,
+                                      enabled: !_loginBack.loading,
+                                    ),
+                                  );
+                                },
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              TextButton(
+                                onPressed: (){
+                                  _loginBack.launchEmail(showModalError);
+                                },
+                                child: const Text(
+                                  'Esqueci a senha',
+                                  style:  TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Observer(
+                                builder: (_) {
+                                  return SizedBox(
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        primary: const Color(0xff237e84),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(5),
+                                        ),
+                                      ),
+                                      child: _loginBack.loading
+                                          ? const CircularProgressIndicator(
+                                              valueColor:
+                                                  AlwaysStoppedAnimation(Colors.white),
+                                            )
+                                          : const Text('Acessar'),
+                                      onPressed: _loginBack.isFormValid
+                                          ? () {
+                                        _loginBack.acessar();
+                                            }
+                                          : null,
+                                    ),
+                                  );
+                                },
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              TextButton(
+                                onPressed: (){
+                                    _loginBack.goToNew(context, 0);
+                                },
+                                child: const Text(
+                                  'Novo cadastro',
+                                  style:  TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                          keyboardType: TextInputType.emailAddress,
-                          onChanged: _loginBack.setEmail,
-                          enabled: !_loginBack.loading,
                         ),
-                      );
-                    },
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Observer(
-                    builder: (_) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        padding: const EdgeInsets.only(left: 16),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Senha',
-                            border: InputBorder.none,
-                            prefixIcon: const Icon(Icons.lock, color: Color(0xff237e84)),
-                            suffixIcon: IconButton(
-                              icon: _loginBack.passVisible
-                                  ? const Icon(Icons.visibility_off, color: Color(0xff237e84))
-                                  : const Icon(Icons.visibility, color: Color(0xff237e84)),
-                              onPressed: _loginBack.togglePassVisible,
-                            ),
-                          ),
-                          obscureText: !_loginBack.passVisible,
-                          onChanged: _loginBack.setSenha,
-                          enabled: !_loginBack.loading,
-                        ),
-                      );
-                    },
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  TextButton(
-                    onPressed: (){
-                      _loginBack.launchEmail(showModalError);
-                    },
-                    child: const Text(
-                      'Esqueci a senha',
-                      style:  TextStyle(
-                        color: Colors.black,
-                        fontSize: 15
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Observer(
-                    builder: (_) {
-                      return SizedBox(
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: const Color(0xff237e84),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          child: _loginBack.loading
-                              ? const CircularProgressIndicator(
-                                  valueColor:
-                                      AlwaysStoppedAnimation(Colors.white),
-                                )
-                              : const Text('Acessar'),
-                          onPressed: _loginBack.isFormValid
-                              ? () {
-                            _loginBack.acessar();
-                                }
-                              : null,
-                        ),
-                      );
-                    },
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  TextButton(
-                    onPressed: (){
-                        _loginBack.goToNew(context, 0);
-                    },
-                    child: const Text(
-                      'Novo cadastro',
-                      style:  TextStyle(
-                        color: Colors.black,
-                        fontSize: 15
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
           ),
         ),
       ),
